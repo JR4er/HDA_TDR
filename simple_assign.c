8878f3c12b9d1f911b33a2d28e3d672c35f8a597
Jack River <jack.zz.river@gmail.com>
2017-11-09 13:45:29 +0800

    use same parameters with sort_assign

--- simple_assign.c
+++ simple_assign.c
@@ -1,6 +1,6 @@
 #include "graph.h"
 
-vertex_t* simple_assign(vertex_t* seq, vertex_t* __attribute__((unused)) buffer[]) {
+vertex_t* simple_assign(graph_t* __attribute__((unused)) graph, vertex_t* seq, vertex_t* __attribute__((unused)) buffer[]) {
     unsigned max_size = 0;
     vertex_t* u;
     vertex_t* l = NULL;

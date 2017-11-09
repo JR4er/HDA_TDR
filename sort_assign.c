bc907dd752d5d4d36b777aeae134c5cf37bd4ebc
Jack River <jack.zz.river@gmail.com>
2017-11-09 13:49:19 +0800

    handle both sort graph and sequence

--- sort_assign.c
+++ sort_assign.c
@@ -1,17 +1,33 @@
 #include "graph.h"
 
-vertex_t* sort_assign(graph_t* graph, vertex_t* b[]) {
+static
+void graph_to_b(graph_t* graph, vertex_t* b[]) {
+    b[0] = NULL;
     vertex_t* u = graph->vertices;
     size_t n = graph->vertex_count;
-    size_t i = n;
-    unsigned k;
-    while (i--) if ((k = u->size - 1)) {
-        u->next = b[k];
-        b[k] = u->next;
-        u++;
+    while (n--) {
+        unsigned k = u->size - 1;
+        u->next = b[k]; b[k] = u++; 
     }
+}
 
-    vertex_t* seq = NULL;
+static
+void seq_to_b(vertex_t* seq, vertex_t* b[]) {
+    vertex_t* u;
+    while ((u=seq)) {
+        seq = seq->next;
+        unsigned k = u->size - 1;
+        u->next = b[k]; b[k] = u;
+    }
+}
+
+vertex_t* sort_assign(graph_t* graph, vertex_t* seq, vertex_t* b[]) {
+
+    if (seq) seq_to_b(seq, b);
+    else graph_to_b(graph, b);
+
+    vertex_t* u;
+    size_t n = graph->vertex_count;
     vertex_t** end = &seq;
     while (--n) while ((u=b[n])) {
         b[n] = u->next;
